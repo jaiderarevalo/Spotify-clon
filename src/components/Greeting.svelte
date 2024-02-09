196097b0dfc11ffeb21d8b07edb029e06992f33a
jaiderarevalo <jaiderruizx12@gmail.com>
2024-02-09 13:31:42 -0500

    Spotify

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+const currentDate = new Date()
+const currentHour = currentDate.getHours()
+
+let greeting = ""
+if(currentHour< 12){
+greeting = 'Buenos Días'
+}else if(currentDate < 18){
+    greeting = 'Buenas tardes'
+}else{
+    greeting = 'Buenas noches'
+}
+
+</script>
+
+<h1 class="text-3xl font-bold" >{greeting}</h1>
